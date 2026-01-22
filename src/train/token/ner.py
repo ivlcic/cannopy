779ab19432e3ba917d42b0ba078cddcb73c72920
Nikola Ivačič <nikola.ivacic@dropchop.com>
2026-01-22 18:17:04 +0100

    Fixing stuff...

--- src/train/token/ner.py
+++ src/train/token/ner.py
@@ -172,7 +172,7 @@ def main(data_args: DataArguments, model_args: ModelArguments, train_args: Train
 
     train_result = trainer.train()
     trainer.save_model(train_args.output_dir)
-    logger.info('Training complete; global steps=%s, training_loss=%.4f',
+    logger.info('Training complete; global steps=%s, training_loss=%.6f',
                 train_result.global_step, train_result.training_loss)
 
     metrics = trainer.evaluate()
